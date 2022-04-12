5fbbabf8cf29a40bbb62be5f4b4b934429b71925
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-04-12 17:43:55 +0200

    Aggiunto parseInt nel jsnack1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -3,15 +3,15 @@
  L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore
 */
 
-const number1 = prompt("Inserisci un numero a scelta");
-console.log("Primo numero selezionato: ", number1);
+const number1 = parseInt(prompt("Inserisci un numero a scelta"));
+console.log("Primo numero selezionato: ", typeof number1, number1);
 
-const number2 = prompt("Per favore inseriscine un altro");
-console.log("Secondo numero selezionato: ",number2);
+const number2 = parseInt(prompt("Per favore inseriscine un altro"));
+console.log("Secondo numero selezionato: ", typeof number2, number2);
 
 
 if (number1 > number2){
-  console.log("Ecco il numero più grande: ", number1);
+  console.log("Ecco il numero più grande: ", typeof number1, number1);
 } else {
-  console.log("Ecco il numero più grande: ", number2);
+  console.log("Ecco il numero più grande: ", typeof number2, number2);
 }
